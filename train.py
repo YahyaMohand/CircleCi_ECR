dfaf81dc1b415721d8445b3ab4433066cdbf5c86
YahyaMohand <96312354+YahyaMohand@users.noreply.github.com>
2024-04-05 16:16:37 +0300

    add the model path

--- train.py
+++ train.py
@@ -1,15 +1,14 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import joblib
+import os
 
-# Load the preprocessed data
 df = pd.read_csv('data/processed_data.csv')
 
 # Split the data into features and target
 X = df.drop('total_salse', axis=1)
 y = df['total_salse']
 
-# Train a simple linear regression model
 model = LinearRegression()
 model.fit(X, y)
 
